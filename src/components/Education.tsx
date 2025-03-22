bea69f696c14617119996b1fefc9c402eea78738
Rezwan Ahmad Nayreed <87903538+RA-Nayreed@users.noreply.github.com>
2025-03-22 19:47:07 +0200

    Update Education.tsx

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -23,7 +23,7 @@ const Education = () => {
       ],
       courses: [
         { name: 'Calculus I', grade: '4' },
-        { name: 'Elementry Programming', grade: '3' },
+        { name: 'Elementary Programming', grade: '3' },
         { name: 'Introduction to Electronics', grade: '4' },
         { name: 'Electrical Measurement Principles', grade: '5' },
         { name: 'Matrix Algebra', grade: '5' },
