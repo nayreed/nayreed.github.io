0e88499c14e8792446879ca731ec413f1ff57493
Rezwan Ahmad Nayreed <nayreedptk@gmail.com>
2025-03-23 17:50:24 +0200

    minors

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -19,7 +19,7 @@ const Skills = () => {
       icon: <img src={ProgrammingSkillsIcon} alt="Programming Skills Icon" className="w-6 h-6" />,
       lucideIcon: <Code size={20} className="text-primary" />,
       skills: [
-        'Python', 'C++', 'MATLAB', 'KiCAD', 'HTML5', 'CSS', 'JavaScript', 'React', 'Node.js', 'GitHub', 'PowerShell', 'WordPress', 'Shopify', 'Microsoft Office'
+        'Python', 'C++', 'MATLAB', 'KiCAD', 'HTML5', 'CSS', 'JavaScript', 'React', 'Node.js', 'GitHub', 'PowerShell', 'WordPress', 'Shopify'
       ]
     },
     {
@@ -27,7 +27,7 @@ const Skills = () => {
       icon: <img src={ProfessionalSkillsIcon} alt="Professional Skills Icon" className="w-6 h-6" />,
       lucideIcon: <Briefcase size={20} className="text-primary" />,
       skills: [
-        'Student tutor', 'Digital Strategy', 'Social Media', 'Organization Skills'
+        'Student tutor', 'Digital Strategy', , 'Microsoft Office', 'Social Media', 'Organization Skills'
       ]
     },
     {
