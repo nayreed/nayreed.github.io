9da4d7957a1ee7f0b2a5280dff112f24e0e44619
Rezwan Ahmad Nayreed <nayreedptk@gmail.com>
2025-05-09 04:25:24 +0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -124,9 +124,9 @@ const Hero = () => {
               View Experience
             </a>
             <a
-              href="https://unioulu-my.sharepoint.com/my?id=%2Fpersonal%2Frnayreed24%5Fstudent%5Foulu%5Ffi%2FDocuments%2FFiles%2FResume%2Epdf&parent=%2Fpersonal%2Frnayreed24%5Fstudent%5Foulu%5Ffi%2FDocuments%2FFiles&ga=1" // File path to your resume
+              href="https://unioulu-my.sharepoint.com/personal/rnayreed24_student_oulu_fi/_layouts/15/Doc.aspx?sourcedoc=%2Fpersonal%2Frnayreed24_student_oulu_fi%2FDocuments%2FFiles%2FResume%2Epdf&file=Resume.pdf&action=download&download=1"
               className="bg-white/5 hover:bg-white/10 backdrop-blur-sm border border-white/10 text-white font-medium rounded-md px-6 py-3 transition-all duration-300 flex items-center secret-resume-button"
-              download="Rezwan_Ahmad_Nayreed_Resume.pdf" // This will suggest the filename for the downloaded file
+              download="Rezwan_Ahmad_Nayreed_Resume.pdf"
               data-tooltip="Download my resume"
             >
               <Download size={16} className="mr-2" />
