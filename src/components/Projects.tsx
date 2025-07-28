1ba196b7f10b7a04c53df72ed1892d5a2c96fb8f
Rezwan Ahmad Nayreed <Rezwan.Nayreed@student.oulu.fi>
2025-07-28 18:34:57 +0300

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,7 +11,7 @@ const Projects = () => {
     {
       title: 'Federated Multiarch Machine Learning',
       description: 'A Flower-based federated learning framework',
-      details: 'End-to-end scripts to train MLP, CNN, and SNN models with FedAvg (via Flower), evaluate IID vs. non-IID setups, run inference, and quickly launch or extend experiments.,
+      details: 'End-to-end scripts to train MLP, CNN, and SNN models with FedAvg (via Flower), evaluate IID vs. non-IID setups.,
       tags: ["Federated Learning", "Flower", "Pytorch ", "snntorch", "snn", "Python"],
       link: 'https://github.com/RA-Nayreed/federated-multiarch-ml'
     },
